ca5685d8fd2ee492f34afad77e059ac68fb9cb14
Zheqi Jin <rv22218@bristol.ac.uk>
2025-02-26 13:38:35 +0000

    defend python

--- imp3_core.py
+++ imp3_core.py
@@ -1,5 +1,6 @@
 print("richard is a nice guy")
 
-print("java is better than python")
+print("java is not better than python")
+print("python is the best language in the world. Regute me.")
 
 print("ella gales boyfreind does tranformer neural netroks in c because he hates himelf")
