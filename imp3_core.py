166d504590f9950ef32fcdb660e98e4949829c39
Calvin Yiu <chunnyyiu@hotmail.com>
2025-02-26 13:28:43 +0000

    fixed typos

--- imp3_core.py
+++ imp3_core.py
@@ -2,4 +2,4 @@ print("richard is a nice guy")
 
 print("java is better than python")
 
-print("ella gales boyfreind does tranformer neural netroks in c because he hates himelf")
+print("ella gales boyfriend does tranformer neural networks in c because he hates himself")
