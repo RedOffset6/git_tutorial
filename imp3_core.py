5d201a5d2321893836439b6a5cc79541f6292d4c
RedOffset6 <ben.luke.morgan@gmail.com>
2025-02-26 13:43:53 +0000

    imporved the spambot

--- imp3_core.py
+++ imp3_core.py
@@ -1,8 +1,13 @@
-print("richard is a git")
-print("ben in a nice guy")
 
-print("java is better than python")
+def spammer():
+    while True:
+        print("richard is a git")
+        print("ben in a nice guy")
 
-print("ella gales boyfreind does tranformer neural netroks in c because he hates himelf")
+        print("java is better than python")
 
-print("what the fork?!")
+        print("ella gales boyfreind does tranformer neural netroks in c because he hates himelf")
+
+        print("what the fork?!")
+
+spammer()
